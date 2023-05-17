79118c17cb8c733772f8c12dcecb1b5b99a9dde1
zainab66 <zainab66r@gmail.com>
2023-05-17 14:31:36 -0400

    add Stack component to navbar with initial styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { logo } from '../utils/constants';
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: 'sticky',
+      background: '#000',
+      top: 0,
+      justifyContent: 'space-between',
+    }}
+  ></Stack>
+);
 
 export default Navbar;
