2025e1b24be21d5730840b6d1d553d9dca85444b
zainab66 <zainab66r@gmail.com>
2023-05-17 14:35:08 -0400

    add imports to searchbar and render Paper

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
 
 const Navbar = () => (
   <Stack
@@ -14,7 +15,13 @@ const Navbar = () => (
       top: 0,
       justifyContent: 'space-between',
     }}
-  ></Stack>
+  >
+    {' '}
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>{' '}
+    <SearchBar />
+  </Stack>
 );
 
 export default Navbar;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import { SearchIcon } from '@mui/icons-material';
+
+export default function SearchBar() {
+  return <Paper>SearchBar</Paper>;
+}
