9f94ced29c48a260e7a2dc78d2f68b48ef65beb5
zainab66 <zainab66r@gmail.com>
2023-05-17 16:07:29 -0400

    create VideoCard and ChannelCard components and map data to them to display in feed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './';
 
 export default function Videos({ videos }) {
-  console.log('videos->', videos);
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => {
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>;
+      })}
+    </Stack>
+  );
 }
