6272d5f56b353d4d60a4cc41dd7795bee9d38957
zainab66 <zainab66r@gmail.com>
2023-05-17 15:37:52 -0400

    add Stack and map over categories from the constants file. Display with styling inside Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constants';
+
+const selectedCategory = 'New';
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: 'auto',
+        height: { sx: 'auto', md: '95%' },
+        flexDirection: { md: 'column' },
+      }}
+    >
+      {categories.map((catagory) => (
+        <button
+          className="category-btn"
+          style={{
+            background: catagory.name === selectedCategory && '#Fc1503',
+          }}
+        >
+          <span>{catagory.icon}</span>
+          <span>{catagory.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
 }
