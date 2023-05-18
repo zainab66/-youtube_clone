d516c98cdb9cd9bd806dd9710e75e8bb0581cbf5
zainab66 <zainab66r@gmail.com>
2023-05-17 20:40:16 -0400

    add Box and styling to header of Channel page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -2,9 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  console.log('channelDetail->', ChannelDetail);
+  console.log('cVVideos->', videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(76,9,121,1) 2%, rgba(188,0,255,1) 100%)',
+            zIndex: 10,
+            height: '300px',
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
