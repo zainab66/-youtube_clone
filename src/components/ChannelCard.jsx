eeb00f4283780fadecabc97db37a2b0be73a1fc3
zainab66 <zainab66r@gmail.com>
2023-05-17 20:33:26 -0400

    add ChannelCard detailing and style to display content for channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,5 +1,47 @@
 import React from 'react';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
 
-export default function ChannelCard() {
-  return <div>ChannelCard</div>;
+export default function ChannelCard({ channelDetail }) {
+  return (
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#FFF',
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: '50%',
+              height: '180px',
+              width: '180px',
+              mb: 2,
+              border: '1px solid #e3e3e3',
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snipper?.title}
+            <CheckCircle sx={{ fontSize: '14px', color: 'gray', ml: '5px' }} />
+          </Typography>
+        </CardContent>
+      </Link>
+    </Box>
+  );
 }
