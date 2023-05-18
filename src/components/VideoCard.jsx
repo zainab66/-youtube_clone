d141fdc3a00d425a142faab2f5959c8d6b3c5d8c
zainab66 <zainab66r@gmail.com>
2023-05-17 20:29:40 -0400

    change Card Content from self-closing tag to encompass Link and Typography fixing on-screen issue

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
-import { CheckCircle } from '@mui/icons-material';
+import CheckCircle from '@mui/icons-material/CheckCircle';
 import {
   demoThumbnailUrl,
   demoVideoUrl,
@@ -17,14 +17,39 @@ export default function VideoCard({
   },
 }) {
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { xs: '100%', sm: '358px', md: '320px' },
+        boxShadow: 'none',
+        borderRadius: 0,
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.high?.url}
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 }
