e6c5c0bbcb1942657b14d6eb51aae41de4aa6fb3
zainab66 <zainab66r@gmail.com>
2023-05-17 20:24:59 -0400

    change {} to () within videos.map function - now showing card / channel data

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from '../utils/constants';
 
-export default function VideoCard() {
+export default function VideoCard({ video }) {
   return <div>VideoCard</div>;
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,13 +4,19 @@ import { VideoCard, ChannelCard } from './';
 
 export default function Videos({ videos }) {
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, index) => {
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>;
-      })}
+        </Box>
+      ))}
     </Stack>
   );
 }
