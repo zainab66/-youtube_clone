defc114b36fe0a191271168ead8a4065c7c97cf4
zainab66 <zainab66r@gmail.com>
2023-05-17 20:45:52 -0400

    add channel detail and card files

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { CheckCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
-export default function ChannelCard({ channelDetail }) {
+export default function ChannelCard({ channelDetail, marginTop }) {
   return (
     <Box
       sx={{
@@ -16,6 +16,7 @@ export default function ChannelCard({ channelDetail }) {
         width: { xs: '356px', md: '320px' },
         height: '326px',
         margin: 'auto',
+        marginTop,
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
